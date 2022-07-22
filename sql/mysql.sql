136d8e6726a258992b1d525817471e6583e2cdb6
jectluci <esteafnoject@gmail.com>
2022-07-22 01:38:45 -0500

    modificados:     mysql.sql

--- sql/mysql.sql
+++ sql/mysql.sql
@@ -1,13 +1,38 @@
 
 alter table libro_reclamacion add COLUMN
-  correlativo VARCHAR(4) default 'LRC0'
+  correlativo VARCHAR(4) default 'LRC0';
 
+alter table libro_reclamacion add column 
+respuesta_empresa varchar(200) default 'Sin respuesta';
 
 CREATE VIEW view_reclamos as 
  select 
   concat(correlativo, id_num_declaracion) as correlativo,
-  dni, DATE(fecha) as 'fecha reclamo', 
-  date(date_add(fecha, interval 30 day)) as 'fecha respuesta' 
+  dni, DATE(fecha) as 'fecha_reclamo', 
+  date(date_add(fecha, interval 30 day)) as 'fecha_respuesta' 
   from libro_reclamacion;
 
-select * from view_reclamos where dni = 70817763 and correlativo = "LRC01"
+select * from view_reclamos where dni = "" and correlativo = "";
+
+/**/
+SELECT 
+    id_num_declaracion
+  FROM
+    libro_reclamacion 
+  WHERE dni = indni AND 
+  fecha = (SELECT MAX( fecha) FROM libro_reclamacion)
+  
+/* alter table libro_reclamacion alter column */
+/* fecha_respuesta VARCHAR(50) as (if( */
+/*             null, date_add(fecha,interval 30 day) */
+/*             )) */
+/* CREATE VIEW view_reclamos as */
+/*  select */ 
+/*   concat(correlativo, id_num_declaracion) as correlativo, */
+/*   dni, date(fecha) as fecha, */  
+/*   IFNULL(fecha_respuesta, date(date_add(fecha, interval 30 day))) as fecha_respuesta, */
+/*   estado, respuesta_empresa */  
+/*   from libro_reclamacion; */
+
+/* update view_reclamos set */ 
+/*     fecha_respuesta =  where correlativo ="LRC014" and dni =222 */
